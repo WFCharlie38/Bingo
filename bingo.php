7386148ad050a8cc041344965c360f9d8ce02c56
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-10-08 11:50:47 +0200

    ver cartones correctamente

--- bingo.php
+++ bingo.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bingo</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
     <div name="j1" id="j1" class="carton">
@@ -11,19 +12,19 @@
         rellenar($carton1);
         mostrarCartonHTML($carton1);
         ?>
-    </div><br>
+    </div>
         <div name="j2" id="j2" class="carton">
         <?php
         rellenar($carton4);
         mostrarCartonHTML($carton4);
         ?>
-    </div><br>
+    </div>
         <div name="j3" id="j3" class="carton">
         <?php
         rellenar($carton7);
         mostrarCartonHTML($carton7);
         ?>
-    </div><br>
+    </div>
         <div name="j4" id="j4" class="carton">
         <?php
         rellenar($carton10);
@@ -99,7 +100,7 @@
 
    function mostrarCartonHTML(&$carton)
 {
-    echo "<table border='1' cellpadding='5' cellspacing='0'>";
+    echo "<table>";
 
     for ($i = 0; $i < 3; $i++) {
         echo "<tr>"; // fila
@@ -107,7 +108,7 @@
             $valor = $carton[$i][$j];
             // Si quieres, puedes mostrar vacío si es 0 o null
             $contenido = ($valor === 0 || $valor === null) ? "&nbsp;" : $valor;
-            echo "<td style='text-align:center;'>$contenido</td>";
+            echo "<td>$contenido</td>";
         }
         echo "</tr>";
     }
