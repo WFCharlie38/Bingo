2bb52156dc40f617450be993cf159c27e4ab2f47
Álvaro Claudio de las Mozas <a.claudio0802@gmail.com>
2025-10-08 09:44:20 +0200

    bing.php y CSS

---
+++ bingo.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bingo</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+
+<?php
+    function numero($num){
+        print($num);
+    }
+
+
+?>
+
+<br><br>
+    
+
+    <div name="j1" id="j1" class="carton">
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+
+    </div>
+
+    <div name="j2" id="j2" class="carton">
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+
+    </div>
+
+    <div name="j3" id="j3" class="carton">
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+
+    </div>
+
+    <div name="j4" id="j4" class="carton">
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+            <tr>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+                <td><?php numero(1) ?></td>
+            </tr>
+        </table>
+
+    </div>
+
+    
+</body>
+</html>
